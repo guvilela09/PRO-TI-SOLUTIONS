cb037029253d6dbff2887c1be931138b19d38d45
igoritoc5 <108372437+igoritoc5@users.noreply.github.com>
2022-11-18 00:39:05 -0300

    Update email.js

--- Pro-Ti Solutions/dist/js/email.js
+++ Pro-Ti Solutions/dist/js/email.js
@@ -1,15 +1,18 @@
 
 
 $( document ).ready(function() {
-
+    
     $( "#sendmail" ).submit(function( event ) {
-
+        
+        console.log("teste123");
         $.ajax({
             type: "GET",
             url: url,
-            data: {email: $("#mail").val(), nome: $("#nome").val(), telefone: $("#telefone").val(), company: $("#company").val(), msg: $("#msg").val()},
-            success: success,
-            dataType: "html"
-          });
+            data: {email: $("#mail").val(), nome: $("#nome").val(), telefone: $("#telefone").val(), company: $("#company").val(), msg: $("#msg").val()}
+           })
+        .then(function successCallback(response) {
+        console.log(response);
+            return 0;
+        });  
     });
 });
