5fddd14aa4bf3c78c1745e7426659e6a3d11828a
igoritoc5 <108372437+igoritoc5@users.noreply.github.com>
2022-11-18 00:45:23 -0300

    Update email.js

--- Pro-Ti Solutions/dist/js/email.js
+++ Pro-Ti Solutions/dist/js/email.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
         console.log("teste123");
         $.ajax({
             type: "GET",
-            url: url,
+            url: "https://send-email-php-hkaw.vercel.app/index.php",
             data: {email: $("#mail").val(), nome: $("#nome").val(), telefone: $("#telefone").val(), company: $("#company").val(), msg: $("#msg").val()}
            })
         .then(function successCallback(response) {
