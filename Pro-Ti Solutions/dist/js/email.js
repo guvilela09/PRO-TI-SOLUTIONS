4b3354dee2ef227074be17852be26206d43e817f
igoritoc5 <108372437+igoritoc5@users.noreply.github.com>
2022-11-18 00:26:35 -0300

    Add files via upload

---
+++ Pro-Ti Solutions/dist/js/email.js
@@ -0,0 +1,15 @@
+
+
+$( document ).ready(function() {
+
+    $( "#sendmail" ).submit(function( event ) {
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            data: {email: $("#mail").val(), nome: $("#nome").val(), telefone: $("#telefone").val(), company: $("#company").val(), msg: $("#msg").val()},
+            success: success,
+            dataType: "html"
+          });
+    });
+});
